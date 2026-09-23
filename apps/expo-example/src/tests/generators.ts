51ae3e43c29ae46b73dfda7ff9292a3833c36ded
Fausto95 <faustino.kialungila@gmail.com>
2026-09-23 10:26:16 +0200

    test: generate values lazily with generators
    Generator functions, closures and methods; yield, yield* and return;
    lazy start and next() results; for-of, spread and Array.from over
    generators, arrays, sets, strings and map iterators through
    Iterable<T>; exceptions from generator bodies; and break out of a
    for-of running the generator's finally blocks, as JavaScript's
    iterator.return() does.

---
+++ apps/expo-example/src/tests/generators.ts
@@ -0,0 +1,12 @@
+// Generated from packages/compiler/test/e2e/cases/generators.test.js. Do not edit.
+// @ts-nocheck
+export default function run(mod, print, lucentClass, mods) {
+  print(mod.basics());
+  print(mod.lazy());
+  print(mod.earlyExit());
+  print(mod.throwsInside());
+  print(mod.tree());
+  print(mod.closures());
+  print(mod.iterables());
+
+}
---
+++ packages/compiler/test/e2e/cases/generators.test.js
@@ -0,0 +1,7 @@
+print(mod.basics());
+print(mod.lazy());
+print(mod.earlyExit());
+print(mod.throwsInside());
+print(mod.tree());
+print(mod.closures());
+print(mod.iterables());
